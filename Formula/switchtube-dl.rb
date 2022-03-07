89c42dfdbb31cf92a47f3d53665f5643ead54326
Maurice Panchaud <16801528+panmau@users.noreply.github.com>
2022-03-07 23:26:56 +0100

    0.0.7

--- Formula/switchtube-dl.rb
+++ Formula/switchtube-dl.rb
@@ -7,7 +7,7 @@ class SwitchtubeDl < Formula
   on_macos do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/panmau/switchtube-dl/releases/download/0.0.7/switchtube-dl.0.0.7.osx-x64.tar.gz"
-      sha256 "29f629623946d13ca21b2701e80c8ff9003300ff5b4151340ac9d0120b6ea859"
+      sha256 "ca9d3155bfceca873e782f471c1acc62cca04c91d3e11b723f926134ef65b63a"
 
       def install
         cp_r '.', bin
@@ -16,7 +16,7 @@ class SwitchtubeDl < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/panmau/switchtube-dl/releases/download/0.0.7/switchtube-dl.0.0.7.osx-arm64.tar.gz"
-      sha256 "53fa03e9ed586db5d2b30302cbe0712f9af61962628b58b26bc0edd8fa5b8ba3"
+      sha256 "d84faed55eb9c1ad840761edbdc43d60e695ff2bca6a19a8a69dbe08a9f443af"
 
       def install
         cp_r '.', bin
@@ -27,7 +27,7 @@ class SwitchtubeDl < Formula
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/panmau/switchtube-dl/releases/download/0.0.7/switchtube-dl.0.0.7.linux-x64.tar.gz"
-      sha256 "490c219b43766b346575859830a03d128ce57e6798cfa34c8e374f2d722f4d87"
+      sha256 "214aad8d15492688c409c7843981d942a391a61529e0f1607d1e61b4a1a61ddd"
 
       def install
         cp_r '.', bin
@@ -36,7 +36,7 @@ class SwitchtubeDl < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_32_bit?
       url "https://github.com/panmau/switchtube-dl/releases/download/0.0.7/switchtube-dl.0.0.7.linux-arm.tar.gz"
-      sha256 "6c43f04f7cbf77a57fce7fd7108e75692ff5f9fb74dc268c146312de16f10ffc"
+      sha256 "fbdc137382b602b6b4af102c256ac2c68e36fcd01c59fc96bb1ddf767fffd32f"
 
       def install
         cp_r '.', bin
@@ -45,7 +45,7 @@ class SwitchtubeDl < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/panmau/switchtube-dl/releases/download/0.0.7/switchtube-dl.0.0.7.linux-arm64.tar.gz"
-      sha256 "aaac1021863893bf5e22a774fb83204dd71faf222c9ea1eeb5777d9d498c44eb"
+      sha256 "05ed86430a2dad8ec17b983977ebd7734a101feac8c9d578567eaf74d38d766e"
 
       def install
         cp_r '.', bin
